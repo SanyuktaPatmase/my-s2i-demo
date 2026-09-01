a32de26f6034b137fc38a26b88914266b6393ba6
SanyuktaPatmase <153738819+SanyuktaPatmase@users.noreply.github.com>
2026-09-02 02:09:10 +0530

    Fix indentation in app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-   from flask import Flask
+from flask import Flask
 
-   app = Flask(__name__)
+app = Flask(__name__)
 
-   @app.route("/")
-   def home():
-       return "Hello! This app was built using S2I (Source-to-Image). No Dockerfile was written!"
+@app.route("/")
+def home():
+    return "Hello! This app was built using S2I (Source-to-Image). No Dockerfile was written!"
 
-   if __name__ == "__main__":
-       app.run(host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
