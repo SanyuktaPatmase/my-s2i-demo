2863dc951366cb95fd005b7a4a670947f2786c3d
SanyuktaPatmase <153738819+SanyuktaPatmase@users.noreply.github.com>
2026-09-16 18:28:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! VERSION 3 - Watching a Rolling Update happen live!"
+    return "Hello! VERSION 4 - This one used Recreate strategy!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
