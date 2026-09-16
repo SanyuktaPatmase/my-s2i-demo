3194d899016f51b016463a99ed8d004ee0911f96
SanyuktaPatmase <153738819+SanyuktaPatmase@users.noreply.github.com>
2026-09-16 14:26:43 +0530

    Update home route message to reflect new version

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! This app was built using S2I (Source-to-Image). No Dockerfile was written!"
+    return "Hello! VERSION 3 - Watching a Rolling Update happen live!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
