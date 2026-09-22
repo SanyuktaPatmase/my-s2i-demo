aa13ba02022a4837844b71511185bf5197e0ab03
SanyuktaPatmase <153738819+SanyuktaPatmase@users.noreply.github.com>
2026-09-22 18:22:18 +0530

    Update home route message to VERSION 3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! VERSION 2 - This proves BuildConfig and ImageStream work together!"
+    return "Hello! VERSION 3 - Watching a Rolling Update happen live!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
